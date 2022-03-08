6a93422841b1de54f60fa98a24e18672e52c0895
Guzman <Guzman@DESKTOP-29GVUO5>
2022-03-08 11:40:51 -0600

    Agregar archivos de proyecto.

---
+++ LabMultiplos5/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Double
+        a = TextBox1.Text
+        If a Mod 5 Then
+            TextBox2.Text = "No es un multiplo"
+        Else
+            TextBox2.Text = "Si es multiplo"
+        End If
+    End Sub
+End Class
